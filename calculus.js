3fa75e45f7744404727cb585ed31175c6bf3475e
W. Scott Stornetta <stornetta@mathisasport.org>
2015-12-29 21:07:17 -0500

    created an array of objects to store filename, correct answer and category for each question.  Clicking on submit increments the array counter, producing a different question on each successive click.

--- calculus.js
+++ calculus.js
@@ -1,23 +1,19 @@
+  var questions = [
+      {filename: '"cb1969ab3.png"', answer: "B",category: 11},
+      {filename: '"cb1969ab7.png"', answer: "D",category: 21},
+      {filename: '"cb1969ab9.png"', answer: "C",category: 24}
+  ];  
 
+  var question_number = 0;
 
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
-  alert('one step before question container assignment');
-  $('#question_container').html('<img src="cb1969ab3.png"/>');
-  alert('one step after question container');
+
   Template.hello.helpers({
     counter: function () {
       return Session.get('counter');
     }
-  });
-
-Template.body.helpers({
-    questions: [
-      { filename: "cb1969ab3.png" },
-      { filename: "cb1969ab3.png" },
-      { filename: "cb1969ab3.png" }
-    ]
   });
 
   Template.body.events({  
@@ -26,7 +22,8 @@ Template.body.helpers({
 //  if element.length() == 0, haven't clicked anything.  submit should remain
 //  greyed out until something is chosen.
     console.log($(element).val());
-    $('#question_container').html('<img src="cb1969ab3.png"/>');
+    question_number = question_number+1;	  
+    $('#question_container').html('<img src=' + questions[question_number].filename +'/>');
 //  compare chosen value to correct answer and move to next problem.
 //  have graphics of three problems with corresponding answer.
 
